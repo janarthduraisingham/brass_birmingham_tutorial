3af7866ce7b0d0db8597bc41f873477102e0739b
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-09-19 21:13:33 +0100

    add intro video to app

--- pages/eliza.py
+++ pages/eliza.py
@@ -12,6 +12,11 @@ video_file = open("videos/eliza_tinsley.mp4", "rb")
 test = video_file.read()
 st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
 
+st.header("Be Eliza, and Take the Industrial Revolution")
+
+video_file = open("videos/intro.mp4", "rb")
+test = video_file.read()
+st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
 
 st.subheader("Next up:")
 if st.button("The Game Board"):
