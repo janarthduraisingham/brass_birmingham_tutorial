0c1bce620fffd59af0c63415f8225b7e46842185
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-09-20 16:06:25 +0100

    add overbuilding self and others' videos

--- pages/build.py
+++ pages/build.py
@@ -15,6 +15,7 @@ st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, 
 
 st.write("Note: if a location has multiple spaces for your industry tile of choice, you must choose the space showing only that industry")
 st.write("Note: Locations with no name cannot be built on with Location Wildcards")
+st.write("Note: in the Canal Era, you may only build one industry tile in one location. In the Railway Era, you may build multiple industry tiles in the same location (though not in the same slot)")
 
 st.subheader("Costs and Requisites")
 video_file = open("videos/costs_requisites.mp4", "rb")
@@ -31,8 +32,16 @@ video_file = open("videos/first_build.mp4", "rb")
 test = video_file.read()
 st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
 
-st.write("Note: in the Canal Era, you may only build one industry tile in one location. In the Railway Era, you may build multiple industry tiles in the same location (though not in the same slot)")
 
+st.subheader("Overbuilding your Tiles")
+video_file = open("videos/overbuilding_self.mp4", "rb")
+test = video_file.read()
+st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
+
+st.subheader("Overbuilding Other Players' Tiles")
+video_file = open("videos/overbuilding_others.mp4", "rb")
+test = video_file.read()
+st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
 
 st.subheader("Next up:")
 if st.button("The Sell Action"):
