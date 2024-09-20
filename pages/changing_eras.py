96bcdc23006b0a163bcc7c22d82a11d4ff21d70d
Janarth Duraisingham <jduraisingham108@gmail.com>
2024-09-20 12:29:13 +0100

    fix: add changing_eras.py placeholder

---
+++ pages/changing_eras.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 00:32:30 2024
+
+@author: jd_se
+"""
+import streamlit as st
+
+st.header("Turns")
+
+video_file = open("videos/turns.mp4", "rb")
+test = video_file.read()
+st.video(test, format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
+
+
+st.subheader("Next up:")
+if st.button("Rounds"):
+    st.switch_page("pages/rounds.py")
+    
+st.write("Went on a tangent? Return to:")
+
+if st.button("Eras"):
+    st.switch_page("pages/eras.py")
+    
